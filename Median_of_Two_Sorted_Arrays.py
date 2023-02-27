099a44627ab321e834b969e8d971865f57b942ce
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-02-27 20:37:39 +0000

    Add solution for Median of Two Sorted Arrays

--- Median_of_Two_Sorted_Arrays.py
+++ Median_of_Two_Sorted_Arrays.py
@@ -1,7 +1,7 @@
 # Problem: Median of Two Sorted Arrays
-# Submission Date: 2023-02-27 20:40:02 UTC
+# Submission Date: 2023-02-27 20:37:39 UTC
 # Language: python3
-# Runtime: 89 ms
-# Memory: 14.1 MB
+# Runtime: 100 ms
+# Memory: 14.3 MB
 # Status: Accepted
 
