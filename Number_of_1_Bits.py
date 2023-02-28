a0a0fc2c91829a380bd6ac083b763b2fe65bbc8e
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-02-28 11:36:52 +0000

    Add solution for Number of 1 Bits

--- Number_of_1_Bits.py
+++ Number_of_1_Bits.py
@@ -1,7 +1,7 @@
 # Problem: Number of 1 Bits
-# Submission Date: 2023-11-29 13:29:59 UTC
+# Submission Date: 2023-02-28 11:36:52 UTC
 # Language: python3
-# Runtime: 35 ms
-# Memory: 16.1 MB
+# Runtime: 29 ms
+# Memory: 13.8 MB
 # Status: Accepted
 
