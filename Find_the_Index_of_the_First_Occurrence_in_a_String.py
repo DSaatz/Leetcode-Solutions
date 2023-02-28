43ff6736a3618b9f21d7bf6e0cf8fcb545901c5f
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-02-28 19:27:57 +0000

    Add solution for Find the Index of the First Occurrence in a String

---
+++ Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -0,0 +1,7 @@
+# Problem: Find the Index of the First Occurrence in a String
+# Submission Date: 2023-02-28 19:27:57 UTC
+# Language: python3
+# Runtime: 32 ms
+# Memory: 13.9 MB
+# Status: Accepted
+
