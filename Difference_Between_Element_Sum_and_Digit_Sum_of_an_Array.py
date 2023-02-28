735d640cfe9afd1f0c53446ea915d36b1a6e9ecf
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-02-28 15:44:07 +0000

    Add solution for Difference Between Element Sum and Digit Sum of an Array

---
+++ Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.py
@@ -0,0 +1,7 @@
+# Problem: Difference Between Element Sum and Digit Sum of an Array
+# Submission Date: 2023-02-28 15:44:07 UTC
+# Language: python3
+# Runtime: 172 ms
+# Memory: 14.5 MB
+# Status: Accepted
+
