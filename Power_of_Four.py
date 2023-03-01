dd61709c461d4bba65ace31e4ada86b1bbbe9c72
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-03-01 16:13:54 +0000

    Add solution for Power of Four

--- Power_of_Four.py
+++ Power_of_Four.py
@@ -1,7 +1,7 @@
 # Problem: Power of Four
-# Submission Date: 2023-10-23 11:16:29 UTC
+# Submission Date: 2023-03-01 16:13:54 UTC
 # Language: python3
-# Runtime: 38 ms
-# Memory: 16.2 MB
+# Runtime: 31 ms
+# Memory: 13.8 MB
 # Status: Accepted
 
