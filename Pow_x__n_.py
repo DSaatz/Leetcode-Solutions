29a4a870faef3108c0531e10847823cc8099feff
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-03-02 19:30:42 +0000

    Add solution for Pow(x, n)

--- Pow_x__n_.py
+++ Pow_x__n_.py
@@ -1,7 +1,7 @@
 # Problem: Pow(x, n)
-# Submission Date: 2023-03-02 19:31:28 UTC
+# Submission Date: 2023-03-02 19:30:42 UTC
 # Language: python3
-# Runtime: 28 ms
+# Runtime: 27 ms
 # Memory: 13.9 MB
 # Status: Accepted
 
