64362a37907ee23b9b8768566fb87e458620a08a
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-06-22 12:33:22 +0000

    Add solution for Kids With the Greatest Number of Candies

--- Kids_With_the_Greatest_Number_of_Candies.py
+++ Kids_With_the_Greatest_Number_of_Candies.py
@@ -1,7 +1,7 @@
 # Problem: Kids With the Greatest Number of Candies
-# Submission Date: 2023-12-20 10:04:47 UTC
+# Submission Date: 2023-06-22 12:33:22 UTC
 # Language: python3
-# Runtime: 51 ms
-# Memory: 17.3 MB
+# Runtime: 49 ms
+# Memory: 16.4 MB
 # Status: Accepted
 
