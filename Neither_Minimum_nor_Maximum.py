f5f1115bc461021bf51d5f8fbbd61915e1be4cdb
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-06-23 12:18:38 +0000

    Add solution for Neither Minimum nor Maximum

--- Neither_Minimum_nor_Maximum.py
+++ Neither_Minimum_nor_Maximum.py
@@ -1,7 +1,7 @@
 # Problem: Neither Minimum nor Maximum
-# Submission Date: 2023-06-23 12:19:27 UTC
+# Submission Date: 2023-06-23 12:18:38 UTC
 # Language: python3
-# Runtime: 378 ms
+# Runtime: 397 ms
 # Memory: 16.5 MB
 # Status: Accepted
 
