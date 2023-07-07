6c8f68113d383d89489b5917711b2cddafee66a3
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-07-07 12:14:07 +0000

    Add solution for Subtract the Product and Sum of Digits of an Integer

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py
@@ -0,0 +1,7 @@
+# Problem: Subtract the Product and Sum of Digits of an Integer
+# Submission Date: 2023-07-07 12:14:07 UTC
+# Language: python3
+# Runtime: 49 ms
+# Memory: 16.3 MB
+# Status: Accepted
+
