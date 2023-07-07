19984d38137f19bbb85be97b4208851868587117
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-07-07 12:17:22 +0000

    Add solution for Element Appearing More Than 25% In Sorted Array

--- Element_Appearing_More_Than_25__In_Sorted_Array.py
+++ Element_Appearing_More_Than_25__In_Sorted_Array.py
@@ -1,7 +1,7 @@
 # Problem: Element Appearing More Than 25% In Sorted Array
-# Submission Date: 2023-12-11 14:54:34 UTC
+# Submission Date: 2023-07-07 12:17:22 UTC
 # Language: python3
-# Runtime: 1418 ms
-# Memory: 18.1 MB
+# Runtime: 2065 ms
+# Memory: 18.3 MB
 # Status: Accepted
 
