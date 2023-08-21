924b0d231805378b18cf1ccb7ae3209abc74b671
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-08-21 06:46:56 +0000

    Add solution for Number of Employees Who Met the Target

--- Number_of_Employees_Who_Met_the_Target.py
+++ Number_of_Employees_Who_Met_the_Target.py
@@ -1,7 +1,7 @@
 # Problem: Number of Employees Who Met the Target
-# Submission Date: 2023-08-21 06:48:36 UTC
+# Submission Date: 2023-08-21 06:46:56 UTC
 # Language: python3
-# Runtime: 52 ms
-# Memory: 16.2 MB
+# Runtime: 44 ms
+# Memory: 16.3 MB
 # Status: Accepted
 
