4e211c6d1afddbdb1dcca2b3326b74d5e5136e23
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-09-01 12:16:15 +0000

    Add solution for Check if All Characters Have Equal Number of Occurrences

---
+++ Check_if_All_Characters_Have_Equal_Number_of_Occurrences.py
@@ -0,0 +1,7 @@
+# Problem: Check if All Characters Have Equal Number of Occurrences
+# Submission Date: 2023-09-01 12:16:15 UTC
+# Language: python3
+# Runtime: 50 ms
+# Memory: 16.2 MB
+# Status: Accepted
+
