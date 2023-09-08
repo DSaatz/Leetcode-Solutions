8c47f776e44f69d7769deecb1081195ee3e1cefc
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-09-08 09:00:57 +0000

    Add solution for Sort Vowels in a String

--- Sort_Vowels_in_a_String.py
+++ Sort_Vowels_in_a_String.py
@@ -1,7 +1,7 @@
 # Problem: Sort Vowels in a String
-# Submission Date: 2023-11-13 13:14:18 UTC
+# Submission Date: 2023-09-08 09:00:57 UTC
 # Language: python3
-# Runtime: 166 ms
+# Runtime: 161 ms
 # Memory: 19.8 MB
 # Status: Accepted
 
