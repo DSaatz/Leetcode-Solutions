4fb31a0de99a124b1315c101492fac1c39526ebc
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-09-28 08:53:36 +0000

    Add solution for Average Salary Excluding the Minimum and Maximum Salary

---
+++ Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.py
@@ -0,0 +1,7 @@
+# Problem: Average Salary Excluding the Minimum and Maximum Salary
+# Submission Date: 2023-09-28 08:53:36 UTC
+# Language: python3
+# Runtime: 37 ms
+# Memory: 16.1 MB
+# Status: Accepted
+
