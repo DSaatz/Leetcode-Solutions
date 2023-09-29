674f714e7eb2b07259f9bfbd7b965a3e4b68d456
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-09-29 06:36:29 +0000

    Add solution for Prime Number of Set Bits in Binary Representation

---
+++ Prime_Number_of_Set_Bits_in_Binary_Representation.py
@@ -0,0 +1,7 @@
+# Problem: Prime Number of Set Bits in Binary Representation
+# Submission Date: 2023-09-29 06:36:29 UTC
+# Language: python3
+# Runtime: 454 ms
+# Memory: 16.1 MB
+# Status: Accepted
+
