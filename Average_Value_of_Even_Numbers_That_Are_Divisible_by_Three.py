c06a7ba9cfc4c188af36f85c815bdeed42e95abe
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-10-06 09:46:01 +0000

    Add solution for Average Value of Even Numbers That Are Divisible by Three

---
+++ Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.py
@@ -0,0 +1,7 @@
+# Problem: Average Value of Even Numbers That Are Divisible by Three
+# Submission Date: 2023-10-06 09:46:01 UTC
+# Language: python3
+# Runtime: 85 ms
+# Memory: 16.3 MB
+# Status: Accepted
+
