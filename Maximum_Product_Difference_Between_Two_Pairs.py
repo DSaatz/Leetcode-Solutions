ddf53fca80609da955ac101963ca956cd2c5246f
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-10-06 06:55:11 +0000

    Add solution for Maximum Product Difference Between Two Pairs

--- Maximum_Product_Difference_Between_Two_Pairs.py
+++ Maximum_Product_Difference_Between_Two_Pairs.py
@@ -1,7 +1,7 @@
 # Problem: Maximum Product Difference Between Two Pairs
-# Submission Date: 2023-10-06 06:56:25 UTC
+# Submission Date: 2023-10-06 06:55:11 UTC
 # Language: python3
-# Runtime: 158 ms
-# Memory: 17.6 MB
+# Runtime: 156 ms
+# Memory: 17.7 MB
 # Status: Accepted
 
