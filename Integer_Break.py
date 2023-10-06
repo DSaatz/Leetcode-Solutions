40ce00b93ec9314c88f98218a9a6447deb2ad0d4
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-10-06 05:36:50 +0000

    Add solution for Integer Break

--- Integer_Break.py
+++ Integer_Break.py
@@ -1,7 +1,7 @@
 # Problem: Integer Break
-# Submission Date: 2023-10-06 05:37:57 UTC
+# Submission Date: 2023-10-06 05:36:50 UTC
 # Language: python3
-# Runtime: 38 ms
-# Memory: 16.1 MB
+# Runtime: 45 ms
+# Memory: 16.2 MB
 # Status: Accepted
 
