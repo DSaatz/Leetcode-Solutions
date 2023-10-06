300223189d9a94b50d9f7f0d937a542d1b41eca5
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-10-06 11:48:35 +0000

    Add solution for Check If Two String Arrays are Equivalent

--- Check_If_Two_String_Arrays_are_Equivalent.py
+++ Check_If_Two_String_Arrays_are_Equivalent.py
@@ -1,7 +1,7 @@
 # Problem: Check If Two String Arrays are Equivalent
-# Submission Date: 2023-12-01 08:17:35 UTC
+# Submission Date: 2023-10-06 11:48:35 UTC
 # Language: python3
-# Runtime: 44 ms
+# Runtime: 39 ms
 # Memory: 16.3 MB
 # Status: Accepted
 
