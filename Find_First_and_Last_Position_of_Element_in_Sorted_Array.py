80aeca739bb84acc35ad338903647b23a1fc445b
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-10-09 13:27:50 +0000

    Add solution for Find First and Last Position of Element in Sorted Array

---
+++ Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,7 @@
+# Problem: Find First and Last Position of Element in Sorted Array
+# Submission Date: 2023-10-09 13:27:50 UTC
+# Language: python3
+# Runtime: 85 ms
+# Memory: 17.7 MB
+# Status: Accepted
+
