85e00a215fb1136a3e333936de97e8cfb57f5f3f
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-10-15 18:40:25 +0000

    Add solution for Keep Multiplying Found Values by Two

--- Keep_Multiplying_Found_Values_by_Two.py
+++ Keep_Multiplying_Found_Values_by_Two.py
@@ -1,7 +1,7 @@
 # Problem: Keep Multiplying Found Values by Two
-# Submission Date: 2023-10-15 18:40:49 UTC
+# Submission Date: 2023-10-15 18:40:25 UTC
 # Language: python3
-# Runtime: 72 ms
-# Memory: 16.4 MB
+# Runtime: 61 ms
+# Memory: 16.5 MB
 # Status: Accepted
 
