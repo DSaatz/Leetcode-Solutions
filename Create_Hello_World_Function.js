a34d917f9b52b486e45a4b10b0564a9edce36300
DSaatz <126864831+DSaatz@users.noreply.github.com>
2023-12-22 15:21:01 +0000

    Add solution for Create Hello World Function

--- Create_Hello_World_Function.js
+++ Create_Hello_World_Function.js
@@ -1,7 +1,7 @@
 # Problem: Create Hello World Function
-# Submission Date: 2023-12-22 15:21:24 UTC
+# Submission Date: 2023-12-22 15:21:01 UTC
 # Language: javascript
-# Runtime: 48 ms
-# Memory: 42.2 MB
+# Runtime: 61 ms
+# Memory: 41.9 MB
 # Status: Accepted
 
