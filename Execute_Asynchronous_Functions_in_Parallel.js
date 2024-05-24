6efe2bc6d337fb73ffb19e236fc2798d359d161f
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-05-24 13:08:51 +0000

    Add solution for Execute Asynchronous Functions in Parallel

--- Execute_Asynchronous_Functions_in_Parallel.js
+++ Execute_Asynchronous_Functions_in_Parallel.js
@@ -1,7 +1,7 @@
 # Problem: Execute Asynchronous Functions in Parallel
-# Submission Date: 2024-05-24 13:09:17 UTC
+# Submission Date: 2024-05-24 13:08:51 UTC
 # Language: javascript
-# Runtime: 67 ms
-# Memory: 50 MB
+# Runtime: 73 ms
+# Memory: 49.7 MB
 # Status: Accepted
 
