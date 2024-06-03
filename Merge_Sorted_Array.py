3ce9997afc78d890fc34787b2fd2257d1094d9cc
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-06-03 14:58:23 +0000

    Add solution for Merge Sorted Array

--- Merge_Sorted_Array.py
+++ Merge_Sorted_Array.py
@@ -1,7 +1,7 @@
 # Problem: Merge Sorted Array
-# Submission Date: 2024-06-03 14:58:33 UTC
+# Submission Date: 2024-06-03 14:58:23 UTC
 # Language: python3
-# Runtime: 43 ms
-# Memory: 16.7 MB
+# Runtime: 37 ms
+# Memory: 16.5 MB
 # Status: Accepted
 
