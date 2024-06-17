08dd5b83fdf4f9b34457f071f6a46db5026494f8
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-06-17 13:44:10 +0000

    Add solution for Count Number of Pairs With Absolute Difference K

---
+++ Count_Number_of_Pairs_With_Absolute_Difference_K.py
@@ -0,0 +1,7 @@
+# Problem: Count Number of Pairs With Absolute Difference K
+# Submission Date: 2024-06-17 13:44:10 UTC
+# Language: python3
+# Runtime: 149 ms
+# Memory: 16.6 MB
+# Status: Accepted
+
