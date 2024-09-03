3a1c228c29c46df39fc0e47ea7445bd85d0584cd
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-09-03 18:28:59 +0000

    Add solution for Count Integers With Even Digit Sum

--- Count_Integers_With_Even_Digit_Sum.py
+++ Count_Integers_With_Even_Digit_Sum.py
@@ -1,7 +1,7 @@
 # Problem: Count Integers With Even Digit Sum
-# Submission Date: 2024-09-03 18:30:35 UTC
+# Submission Date: 2024-09-03 18:28:59 UTC
 # Language: python3
-# Runtime: 41 ms
+# Runtime: 51 ms
 # Memory: 16.6 MB
 # Status: Accepted
 
