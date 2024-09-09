7851eb2dd58643ff3dbcf93a8fbc23b17197b543
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-09-09 07:43:51 +0000

    Add solution for Maximum Number of Vowels in a Substring of Given Length

---
+++ Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.py
@@ -0,0 +1,7 @@
+# Problem: Maximum Number of Vowels in a Substring of Given Length
+# Submission Date: 2024-09-09 07:43:51 UTC
+# Language: python3
+# Runtime: 105 ms
+# Memory: 17.4 MB
+# Status: Accepted
+
