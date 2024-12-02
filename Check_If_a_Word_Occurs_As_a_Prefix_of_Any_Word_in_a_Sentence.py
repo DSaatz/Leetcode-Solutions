30b38d9f0cb805913391e5897267366b041a27cb
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-02 12:52:01 +0000

    Add solution for Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.py
@@ -0,0 +1,7 @@
+# Problem: Check If a Word Occurs As a Prefix of Any Word in a Sentence
+# Submission Date: 2024-12-02 12:52:01 UTC
+# Language: python3
+# Runtime: 0 ms
+# Memory: 17.1 MB
+# Status: Accepted
+
