be4a0f8d6c4fc44429ebb5c2035db103217f5a49
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-16 08:33:01 +0000

    Add solution for Final Array State After K Multiplication Operations I

---
+++ Final_Array_State_After_K_Multiplication_Operations_I.java
@@ -0,0 +1,7 @@
+# Problem: Final Array State After K Multiplication Operations I
+# Submission Date: 2024-12-16 08:33:01 UTC
+# Language: java
+# Runtime: 3 ms
+# Memory: 44.3 MB
+# Status: Accepted
+
